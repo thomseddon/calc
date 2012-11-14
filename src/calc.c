thomseddon/calc

commit 483d31d6e7a73d4fddd0e4c8c5e1e732202e2cbd

Nomalise operators + add support for both * and x, ' and ^
Some operators do now work nicely with argv, this function normalises the funny business and allows expected characters to be used with passing a string, e.g. "2 * 4 + 3^4" now works (as does 3 x 4 + 3'4)

src/calc.c
@@ -74,11 +74,24 @@ double strtoconst(char *nptr, char **endptr)
 	return replace;
 }
 
+char normalise(char operator)
+{
+	switch (operator) {
+		case 'x':
+		case '*':
+			return '*';
+		case '\'':
+		case '^':
+			return '^';
+		default:
+			return operator;
+	}
+}
 
 double operation(char operator, double pre, double post)
 {
 	switch (operator) {
-		case '\'': return pow(pre, post);
+		case '^': return pow(pre, post);
 		case '/': return pre / post;
 		case 'x': return pre * post;
 		case '+': return pre + post;
@@ -142,7 +155,7 @@ void main(int argc, char *argv[])
 			} else if ((number = strtold(str, &str)) == 0.0L && (number = strtoconst(str, &str)) == 0.0L) {
 				/* NaN */
 				strcpy(equation[equationCounter].type, OP);
-				equation[equationCounter].charVal = *str;
+				equation[equationCounter].charVal = normalise(*str);
 				equationCounter++;
 
 				*str++;
@@ -156,9 +169,9 @@ void main(int argc, char *argv[])
 	}
 
 	/* Evaluate */
-	findOperations('\'', equation, &equationCounter); //bOdmas
+	findOperations('^', equation, &equationCounter); //bOdmas
 	findOperations('/', equation, &equationCounter); //boDmas
-	findOperations('x', equation, &equationCounter); //bodMas
+	findOperations('*', equation, &equationCounter); //bodMas
 	findOperations('+', equation, &equationCounter); //bodmAs
 	findOperations('-', equation, &equationCounter); //bodmaS
 

commit 7fc3ed4f48ede0a4aa8ce767b5e5c49de684fe54

Fix multiplication parsing bug introduced in 483d31d

src/calc.c
@@ -93,7 +93,7 @@ double operation(char operator, double pre, double post)
 	switch (operator) {
 		case '^': return pow(pre, post);
 		case '/': return pre / post;
-		case 'x': return pre * post;
+		case '*': return pre * post;
 		case '+': return pre + post;
 		case '-': return pre - post;
 	}